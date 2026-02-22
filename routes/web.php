fd0238efc5178c067b36177afd16efbb99505683
krisna-yudha <gentungkuy@gmail.com>
2026-02-22 14:11:26 +0700

    ~

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,15 @@ use App\Http\Controllers\DashboardController;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 Route::resource('pendosa', \App\Http\Controllers\PendosaController::class);
+
+// POST route untuk simpan dosa (dari AJAX)
 Route::post('/simpan-dosa', [\App\Http\Controllers\PendosaController::class, 'simpanDosa'])->name('simpan.dosa');
+
+// Fallback GET route (redirect ke dashboard jika diakses langsung)
+Route::get('/simpan-dosa', function() {
+    return redirect()->route('dashboard');
+});
+
 Route::get('/surga', function() {
     return view('pendosa.surga');
 })->name('surga');
